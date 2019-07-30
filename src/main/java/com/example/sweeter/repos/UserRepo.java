1a7ec953e7645554761bab70b25fe59875c550e6
volodymyr <wvlasenko@gmail.com>
2019-07-30 13:21:19 +0300

    commit#1 rebild project

---
+++ src/main/java/com/example/sweeter/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.sweeter.repos;
+
+
+import com.example.sweeter.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
